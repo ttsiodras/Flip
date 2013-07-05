4f3a51725471589683853451d8a37625fe559a2e
Thanassis Tsiodras <ttsiodras@gmail.com>
2013-07-05 14:29:40 +0300

    Cleanup pass.

--- Flip.cc
+++ Flip.cc
@@ -16,9 +16,25 @@ using namespace std;
 // The board is SIZE x SIZE tiles
 const int SIZE = 5;
 
+#define OFS(y,x) (size_t)((y)*SIZE + (x))
+
 // I quickly found out that the puzzle space is big, and memory usage becomes
 // an issue...  This define toggles the original unoptimized code back in:
 //#define NONOPTIMAL_CODE
+
+//////////////////////////////
+//      Types used
+//////////////////////////////
+
+
+// We will need to store the moves done so far, to avoid redoing them,
+// Since memory is at a premium, we use a bitset to check for this
+struct ListOfMoves : bitset<SIZE*SIZE> {
+    ListOfMoves(unsigned long v=0):bitset<SIZE*SIZE>(v) {}
+
+    void addMove(unsigned ofs)           { set(ofs); }
+    bool moveAlreadyPlayed(unsigned ofs) { return test(ofs); }
+};
 
 #ifdef NONOPTIMAL_CODE
 // In the unoptimized version, the board was represented as a vector
@@ -30,18 +46,29 @@ enum TileKind {
 
 struct Board: public vector<TileKind> {
     Board():vector<TileKind>(SIZE*SIZE) {}
-    inline TileKind  get(int y, int x) const { return (*this)[y*SIZE + x]; }
-    inline TileKind& get(int y, int x)       { return (*this)[y*SIZE + x]; }
+    inline bool test(unsigned ofs) const { return (*this)[ofs] == Full; }
+    inline void set(unsigned ofs)        { (*this)[ofs] = Full; }
+    inline void flip(unsigned ofs)       { (*this)[ofs] = test(ofs)?Empty:Full; }
 };
 
 // ...and each move was stored as the y,x coordinates of the tile toggled:
 struct Move {
     enum { Sentinel=0xf };
-    unsigned int _y, _x;
-    inline Move(int y, int x):_y(y), _x(x) {}
+    int _y, _x;
+    inline Move(int yy, int xx):_y(yy), _x(xx) {}
     inline int y() const { return _y; }
     inline int x() const { return _x; }
 };
+
+// The state to store for each move in the Breadth-First-Search:
+
+// - The depth in the move tree that we are currently in
+// - The Move we performed to reach the current state
+// - The Board's current state
+// - The moves we've played to get here (bitset)
+typedef tuple<unsigned char, Move, Board, ListOfMoves> State;
+
+
 #else
 
 // In the optimized version, bitsets are used to represent boards. Basically,
@@ -51,22 +78,22 @@ struct Move {
 // The difference in memory usage is at least an order of magnitude...
 struct Board: public bitset<SIZE*SIZE> {
     Board(unsigned long v=0):bitset<SIZE*SIZE>(v) {}
-    inline bool get(int y, int x) const { return (*this)[y*SIZE + x]; }
-    inline reference get(int y, int x)  { return (*this)[y*SIZE + x]; }
 
     // The Board will be used as a key in a set (visited)
     // In contrast to the vector used in the non-optimized version,
     // the bitset does not provide a default comparison operator,
     // so we had to provide our own:
     inline bool operator<(const Board& rhs) const {
-        //size_t i = SIZE*SIZE-1;
-        //while (i > 0) {
-        //    if ((*this)[i-1] == rhs[i-1])
-        //        i--;
-        //    else
-        //       return (*this)[i-1] < rhs[i-1];
-        //}
-        //return false;
+        // Instead of comparing the bits one by one...
+        //
+        // size_t i = SIZE*SIZE-1;
+        // while (i > 0) {
+        //     if ((*this)[i-1] == rhs[i-1])
+        //         i--;
+        //     else
+        //        return (*this)[i-1] < rhs[i-1];
+        // }
+        // return false;
         //
         // Instead of comparing the bits one by one, we can compare
         // the contained unsigned long - this gave a 100% speedup:
@@ -79,26 +106,54 @@ static_assert(
     sizeof(Board)<=sizeof(unsigned long),
     "Uncomment the full comparison in Board::operator<");
 
-// Finally, squeeze some more memory out of the Move, by storing both
+// Squeeze some more memory out of the Move, by storing both
 // x and y in a single unsigned char's 8 bits (4 for x, 4 for y):
 struct Move {
     enum { Sentinel=0xf };
     unsigned char _yx;
-    inline Move(int y, int x):_yx((y<<4)|x) {}
-    inline Move(unsigned char yx):_yx(yx)   {}
-    inline int y() const                    { return _yx>>4;  }
-    inline int x() const                    { return _yx&0xf; }
+    inline Move(int yy, int xx):_yx((unsigned char)((yy<<4)|xx)) {}
+    inline Move(unsigned char yx):_yx(yx)       {}
+    inline int y() const                        { return _yx>>4;  }
+    inline int x() const                        { return _yx&0xf; }
+};
+
+// For the State placed in the Breadth-First-Search Queue, I had to
+// optimize for memory use - packing all info in 2 unsigned longs:
+struct State {
+    unsigned long _u1, _u2;
+    State(
+        unsigned char level,
+        const Move& move,
+        const Board& board,
+        const ListOfMoves& listOfMoves)
+    {
+        // First unsigned long's 32 bits:
+        //
+        //       <--the level->  <-- The 25 bits of the board -->
+        // 31 30 29 28 27 26 25  24 23 22 21 20 19 18 17 16 ... 0
+        // \___/
+        //   |___ The two upper bits of move._yx
+        //
+        _u1 = (unsigned(move._yx) & 0xC0) << 24;
+        _u1|= (unsigned(level&0x1f) << 25)       | board.to_ulong();
+
+        // Second unsigned long's 32 bits:
+        //
+        //    <-- 6 low bits of move._yx -> <-- The 25 bits of listOfMoves -->
+        // 31 30 29 ...               26 25 24 23 22 21 20 19 18 17 16 15... 0
+        //
+        _u2 = (unsigned(move._yx&0x3f) << 25)    | listOfMoves.to_ulong();
+        //
+        // Oh look - we have one bit to spare :-)
+    }
+
+    unsigned char getLevel()     { return (unsigned char)((_u1>>25)&0x1F); }
+    Move getMove()               { return Move((unsigned char)(_u2>>25|((_u1&0xC0000000)>>24))); }
+
+    Board getBoard()             { return Board      (_u1 & 0x1FFFFFF); }
+    ListOfMoves getListOfMoves() { return ListOfMoves(_u2 & 0x1FFFFFF); }
 };
 #endif
-
-// We will need to store the moves done so far, to avoid redoing them,
-// Since memory is at a premium, use a bitset:
-struct ListOfMoves : bitset<SIZE*SIZE> {
-    ListOfMoves(unsigned long v=0):bitset<SIZE*SIZE>(v) {}
-
-    void addMove(int y, int x)           { (*this)[y*SIZE + x] = true; }
-    bool moveAlreadyPlayed(int y, int x) { return (*this)[y*SIZE+x]; }
-};
 
 // The offsets of tiles to toggle when clicking on a tile:
 const pair<int,int> g_offsetsList[] = {
@@ -112,7 +167,7 @@ void printBoard(const Board& board, Move move)
     for(int i=0; i<SIZE; i++) {
         cout << "|";
         for(int j=0; j<SIZE; j++) {
-            char c = board.get(i,j)?'X':' ';
+            char c = board.test(OFS(i,j))?'X':' ';
             if (move.y() == i && move.x() == j)
                 cout << '[' << c << ']';
             else
@@ -130,51 +185,16 @@ void playMove(Board& board, int y, int x)
         int yy = y + step.first;
         int xx = x + step.second;
         if (yy>=0 && yy<SIZE && xx>=0 && xx<SIZE) {
-#ifdef NONOPTIMAL_CODE
-            board.get(yy, xx) = board.get(yy, xx)==Full?Empty:Full;
-#else
-            board.get(yy, xx) = !board.get(yy, xx);
-#endif
+            board.flip(OFS(yy,xx));
         }
     }
 }
-
-// The state to store for each move in the Breadth-First-Search:
-#ifdef NONOPTIMAL_CODE
-// Initially, I just used a tuple of 4 things:
-// - The depth in the move tree that we are currently in
-// - The Move we performed to reach the current state
-// - The Board's current state
-// - The moves we've played to get here (bitset)
-typedef tuple<int, Move, Board, ListOfMoves> State;
-
-#else
-// Again, I had to optimize for memory use - packing all info in 2 ulong:
-struct State {
-    unsigned long _u1, _u2;
-    State(
-        int level,
-        const Move& move,
-        const Board& board,
-        const ListOfMoves& listOfMoves)
-    {
-        _u1 = (unsigned(level&0x1f) << 25) | board.to_ulong();
-        _u1 |= ((unsigned(move._yx) & 0x80) << 24);
-        _u2 = (move._yx << 25)             | listOfMoves.to_ulong();
-    }
-
-    int getLevel()               { return (int)((_u1 >> 25)&0x1F); }
-    Move getMove()               { return Move(_u2>>25 | ((_u1&0x80000000) >> 24)); }
-    Board getBoard()             { return Board(_u1 & 0x1FFFFFF); }
-    ListOfMoves getListOfMoves() { return ListOfMoves(_u2 & 0x1FFFFFF); }
-};
-#endif
 
 // ...and finally, the brains of the operation:
 // a Breadth-First-Search of the problem space.
 //  ( http://en.wikipedia.org/wiki/Breadth-first_search )
 
-void SolveBoard(Board& board)
+void SolveBoard(Board& initialBoard)
 {
     cout << "\nSearching for a solution...\n\n";
 
@@ -188,7 +208,7 @@ void SolveBoard(Board& board)
     // state - we used no Move to achieve it, so store a special Move
     // marked up with the sentinel value for y,x:
     unsigned char oldLevel = 0;
-    BoardAndLevel key(board, oldLevel);
+    BoardAndLevel key(initialBoard, oldLevel);
     previousMoves.insert(
         pair<BoardAndLevel, Move>(key, Move(Move::Sentinel, Move::Sentinel)));
 
@@ -210,7 +230,7 @@ void SolveBoard(Board& board)
     // Start the Q with our initial board state, depth, a sentinel Move
     // (we used no move to get here) and an empty list of moves so far.
     queue.push_back(
-        State(0, Move(Move::Sentinel, Move::Sentinel), board, ListOfMoves(0)));
+        State(0, Move(Move::Sentinel, Move::Sentinel), initialBoard, ListOfMoves(0)));
 
     while(!queue.empty()) {
 
@@ -234,7 +254,7 @@ void SolveBoard(Board& board)
 
         if (level > oldLevel) {
             // Report depth increase when it happens:
-            cout << "Depth searched: " << setw(2) << level
+            cout << "Depth searched: " << setw(2) << int(level)
                  << ", states to check in Q: " << queue.size() << endl;
             oldLevel = level;
         }
@@ -249,8 +269,8 @@ void SolveBoard(Board& board)
         visited.insert(board);
 
         // Store the move used to get this board, so we can backtrack later
-        BoardAndLevel key(board, oldLevel);
-        previousMoves.insert(pair<BoardAndLevel, Move>(key, move));
+        BoardAndLevel key2(board, oldLevel);
+        previousMoves.insert(pair<BoardAndLevel, Move>(key2, move));
 
         // Check if this board state is a winning state:
 #ifdef NONOPTIMAL_CODE
@@ -259,8 +279,7 @@ void SolveBoard(Board& board)
 
         if (it == board.end()) {
 #else
-        static bitset<SIZE*SIZE> empty;
-        if (board == empty) {
+        if (board.to_ulong() == 0) {
 #endif
             // Yes - we did it!
             cout << "\n\nSolved at depth " << level << "!\n";
@@ -299,14 +318,14 @@ void SolveBoard(Board& board)
         for(int i=0; i<SIZE; i++) {
             for(int j=0; j<SIZE; j++) {
 
-                if (movesSoFar.moveAlreadyPlayed(i,j))
+                if (movesSoFar.moveAlreadyPlayed(OFS(i,j)))
                     continue;
 
                 for (auto& step: g_offsetsList) {
                     int yy = i + step.first;
                     int xx = j + step.second;
                     if (yy>=0 && yy<SIZE && xx>=0 && xx<SIZE) {
-                        if (board.get(yy, xx)) {
+                        if (board.test(OFS(yy,xx))) {
                             Board newBoard = board;
                             playMove(newBoard, i, j);
 
@@ -314,9 +333,9 @@ void SolveBoard(Board& board)
                                 /* Add to the end of the queue for further
                                  * investigation */
                                 auto newMovesSoFar = movesSoFar;
-                                newMovesSoFar.addMove(i, j);
+                                newMovesSoFar.addMove(OFS(i,j));
                                 queue.push_back(
-                                    State(  level+1,
+                                    State(  (unsigned char)(level+1),
                                             Move(i, j),
                                             newBoard,
                                             newMovesSoFar));
@@ -335,42 +354,37 @@ void SolveBoard(Board& board)
 int main()
 {
     Board board;
-    //board.get(0,0) = true;
-    //board.get(0,4) = true;
+    //board.set(OFS(0,0));
+    //board.set(OFS(0,4));
     //
-    //board.get(0,1) = true; board.get(0,4) = true;
-    //board.get(1,3) = true;
-    //board.get(2,0) = true; board.get(2,2) = true; board.get(2,4) = true; board.get(3,0) = true;
-    //board.get(3,2) = true; board.get(3,3) = true; board.get(3,4) = true;
-    //board.get(4,1) = true; board.get(4,3) = true;
+    //board.set(OFS(0,1)); board.set(OFS(0,4));
+    //board.set(OFS(1,3));
+    //board.set(OFS(2,0)); board.set(OFS(2,2)); board.set(OFS(2,4)); board.set(OFS(3,0));
+    //board.set(OFS(3,2)); board.set(OFS(3,3)); board.set(OFS(3,4));
+    //board.set(OFS(4,1)); board.set(OFS(4,3));
     //
-    //board.get(0,1) = true; board.get(0,2) = true; board.get(0,4) = true;
-    //board.get(1,0) = true; board.get(1,4) = true;
-    //board.get(2,0) = true; board.get(2,4) = true;
-    //board.get(3,0) = true; board.get(3,3) = true; board.get(3,4) = true;
-    //board.get(4,0) = true; board.get(4,1) = true; board.get(4,3) = true; board.get(4,4) = true;
+    //board.set(OFS(0,1)); board.set(OFS(0,2)); board.set(OFS(0,4));
+    //board.set(OFS(1,0)); board.set(OFS(1,4));
+    //board.set(OFS(2,0)); board.set(OFS(2,4));
+    //board.set(OFS(3,0)); board.set(OFS(3,3)); board.set(OFS(3,4));
+    //board.set(OFS(4,0)); board.set(OFS(4,1)); board.set(OFS(4,3)); board.set(OFS(4,4));
     //
-    //board.get(0,0) = true; board.get(0,4) = true;
-    //board.get(1,2) = true; board.get(1,3) = true; board.get(1,4) = true;
-    //board.get(2,2) = true; board.get(2,4) = true;
-    //board.get(3,0) = true; board.get(3,2) = true; board.get(3,3) = true; board.get(3,4) = true;
-    //board.get(4,0) = true; board.get(4,1) = true; board.get(4,2) = true; board.get(4,4) = true;
+    //board.set(OFS(0,0)); board.set(OFS(0,4));
+    //board.set(OFS(1,2)); board.set(OFS(1,3)); board.set(OFS(1,4));
+    //board.set(OFS(2,2)); board.set(OFS(2,4));
+    //board.set(OFS(3,0)); board.set(OFS(3,2)); board.set(OFS(3,3)); board.set(OFS(3,4));
+    //board.set(OFS(4,0)); board.set(OFS(4,1)); board.set(OFS(4,2)); board.set(OFS(4,4));
     //
-    //board.get(4,2) = true;
-    //board.get(4,3) = true;
-    //board.get(4,4) = true;
+    //board.set(OFS(4,2));
+    //board.set(OFS(4,3));
+    //board.set(OFS(4,4));
     //
-#ifdef NONOPTIMAL_CODE
-    TileKind onValue = Full;
-#else
-    bool onValue = true;
-#endif
-    board.get(2,0) = onValue;
-    board.get(3,0) = onValue;
-    board.get(3,1) = onValue;
-    board.get(4,1) = onValue;
-    board.get(4,3) = onValue;
-    board.get(4,4) = onValue;
+    board.set(OFS(2,0));
+    board.set(OFS(3,0));
+    board.set(OFS(3,1));
+    board.set(OFS(4,1));
+    board.set(OFS(4,3));
+    board.set(OFS(4,4));
 
     SolveBoard(board);
 }
